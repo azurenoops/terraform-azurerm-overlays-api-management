819c58cea48a9f0964713797349fd8060693c2cf
John Scott (Azure) <joscot@microsoft.com>
2023-05-01 20:21:22 -0700

    App Insights toggle

--- resources.apimanagement.appinsights.tf
+++ resources.apimanagement.appinsights.tf
@@ -1,4 +1,5 @@
-resource "azurerm_application_insights" "apim_app_insights" {
+resource "azurerm_application_insights" "apim_app_insights" {  
+  count               = var.enable_application_insights ? 1 : 0
   location            = local.location
   name                = local.apim_app_insights_name
   workspace_id        = var.log_analytics_workspace_id
@@ -10,12 +11,13 @@ resource "azurerm_api_management_logger" "app_insights" {
     azurerm_api_management.api_management,
     azurerm_application_insights.apim_app_insights
   ]
+  count               = var.enable_application_insights ? 1 : 0
   name                = local.apim_logger_name
   api_management_name = azurerm_api_management.api_management.name
   resource_group_name = local.resource_group_name
 
   application_insights {
-    instrumentation_key = azurerm_application_insights.apim_app_insights.instrumentation_key
+    instrumentation_key = azurerm_application_insights.apim_app_insights[0].instrumentation_key
   }
 }
 
@@ -23,10 +25,11 @@ resource "azurerm_api_management_diagnostic" "app_insights" {
     depends_on = [
         azurerm_api_management_logger.app_insights
     ]
+  count                    = var.enable_application_insights ? 1 : 0
   identifier               = "applicationinsights"
   resource_group_name      = local.resource_group_name
   api_management_name      = azurerm_api_management.api_management.name
-  api_management_logger_id = azurerm_api_management_logger.app_insights.id
+  api_management_logger_id = azurerm_api_management_logger.app_insights[0].id
 
   sampling_percentage       = 5.0
   always_log_errors         = true
